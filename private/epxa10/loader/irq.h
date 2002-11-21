e6b672a06848dabef6b1fe62526619609a035973
Arthur Jones <arthur@lbl.gov>
2002-11-21 05:07:56 -0600

    import altera code...

---
+++ private/epxa10/loader/irq.h
@@ -0,0 +1,16 @@
+/*
+* 	Copyright (c) Altera Corporation 2002.
+* 	All rights reserved.
+*
+*	IRQ.h
+*
+*       Function prototypes and definitions to use the Interrupt handling routines
+*/
+
+#ifndef IRQ_H
+#define IRQ_H
+
+#define INT_CTRL00_TYPE (volatile unsigned int *)
+void irq_init(void);
+
+#endif /* IRQ_H */

c8f016b8d9b214d03bf8fe69d66c8904b8708033
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2003-09-25 10:48:00 -0500

    This commit was manufactured by cvs2svn to create tag 'V01-01-09'.

--- localTargets.mk
+++ localTargets.mk
@@ -19,7 +19,7 @@ $(HOST_BIN_DIR)/mempat : private/epxa10/booter/mempat.c
 	$(HOST_CC) -o $(@) $(<)
 
 $(BUILD_DIR)/crt0.o : crt0.S $(BUILD_DIR)/$(PUB_ROOT)/booter/pte.S $(BUILD_DIR)/$(PUB_ROOT)/booter/epxa.h
-	$(CPP) $(CPP_FLAGS) -o $(@D)/$(*F).i $(<)
+	$(CPP) -I$(BUILD_DIR)/$(PUB_ROOT) -o $(@D)/$(*F).i $(<)
 	$(AS) $(A_FLAGS) $(INC_PATHS) -o $(@) $(@D)/$(*F).i
 
 $(BUILD_DIR)/%.o: %.S

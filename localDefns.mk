33db80a9c2534c8b42e338af978a33ee296ab43b
CVS-to-SVN conversion program <cvs2svn@icecube.wisc.edu>
2004-03-26 10:46:40 -0600

    This commit was manufactured by cvs2svn to create tag 'V02-00-01'.

--- localDefns.mk
+++ localDefns.mk
@@ -3,5 +3,5 @@ COMMON_AWK := common.awk
 ASBL_SUFFIX := S
 #CONFIG_AWK := config.epxa1
 #CONFIG_AWK := config.dom-mb
-CONFIG_AWK := config.dom-mb.micron
-#CONFIG_AWK := config.dom-mb.epxa4
+#CONFIG_AWK := config.dom-mb.micron
+CONFIG_AWK := config.dom-mb.epxa4
